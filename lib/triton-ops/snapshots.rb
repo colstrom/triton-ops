c91a46f26eb93ab59d7ecedee676ef355a6da1cb
Chris Olstrom <chris@olstrom.com>
2018-02-13 13:05:21 -0800

    Adds Wrapper - snapshots
    This wraps the various "snapshot/*" requires for convenience.

---
+++ lib/triton-ops/snapshots.rb
@@ -0,0 +1,8 @@
+
+# -*- ruby -*-
+
+require_relative 'snapshot'
+require_relative 'snapshot/catalog'
+require_relative 'snapshot/collector'
+require_relative 'snapshot/explorer'
+require_relative 'snapshot/reporter'
