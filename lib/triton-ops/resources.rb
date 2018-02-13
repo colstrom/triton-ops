8443af60dfc86300b37c530e4bd4c37fe9a5a910
Chris Olstrom <chris@olstrom.com>
2018-02-13 13:03:12 -0800

    Adds Wrapper - resources
    This wraps the various "resource/*" requires for convenience.

---
+++ lib/triton-ops/resources.rb
@@ -0,0 +1,11 @@
+
+# -*- ruby -*-
+
+require_relative 'resource/image'
+require_relative 'resource/platform'
+require_relative 'resource/server'
+require_relative 'resource/user'
+require_relative 'resource/virtual_machine'
+require_relative 'resource/virtual_machine/disk'
+require_relative 'resource/virtual_machine/network_interface'
+require_relative 'resource/vm'
